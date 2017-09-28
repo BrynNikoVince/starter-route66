615e3773a83503a4b4e1d2ef220425f471aa493d
Niko Arellano <narellano@my.bcit.ca>
2017-09-28 13:20:08 -0700

    Closes #8 Added a route for Foxtrot. This is dependent on Golf

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+// Remap /bananas/rule route to 'golf'
+$route['bananas/rule] = function($fruit,$verb) {
+    return 'golf';
+};
+
